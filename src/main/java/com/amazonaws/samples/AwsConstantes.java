f8a0651b1966f9db93c58a6792a37704a741429a
Charlys <charlysmoreira@gmail.com>
2017-07-10 08:03:03 -0300

    Update AwsConstantes.java
    Ajustes dos acessos

--- src/main/java/com/amazonaws/samples/AwsConstantes.java
+++ src/main/java/com/amazonaws/samples/AwsConstantes.java
@@ -8,12 +8,12 @@ package com.amazonaws.samples;
 public class AwsConstantes {
 	
 	public static final String name = "teste2";
-	public static final String ami_id = "ami-11aa8c74";
+	public static final String ami_id = "<AMI_ID>";
 	
-	public static final String secretKey = "hxi5qTFNdYtfAU5E9nZnxx0Fv/Y/otFqkWF0VS/T";
-	public static final String accessKey = "AKIAJX4TGDZXWZGJEL3A";
+	public static final String secretKey = "<CHAVE_SECRETA>";
+	public static final String accessKey = "<CHAVE_ACESSO>";
 	
 	//TODO Pegar id da instacia atualizado da lista de instancia do console da amazon
-	public static final String NOVA_INSTANCIA_ID = "i-01a3346c3fcd2b081";
+	public static final String NOVA_INSTANCIA_ID = "<ID_NOVA_INSTANCIA_CRIADA>";
 
 }
